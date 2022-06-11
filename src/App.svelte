b8a4fb0985fef0c30b7355f1b8b00bb64b443572
edewindt <edewindt27@outlook.com>
2022-06-11 02:01:23 -0500

    first commit, basic build

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+<main>
+	<legend>Search For Character</legend>
+	<label for="character">Character: </label>
+	<input id="character" type="text" placeholder="type the exact name">
+	<div class="iamge">
+
+	</div>
+	<h2>ID:</h2>
+	<h2>Full Name:</h2>
+	<h2>Family:</h2>
+	<button>Generate Quote</button>
+</main>
+<style>
+	legend{
+		font-size: 2rem;
+	}
+	.iamge{
+		height: 350px;
+		width: 350px;
+		background-color: black;
+	}
+</style>
